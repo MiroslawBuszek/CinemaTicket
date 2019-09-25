042521c63806496a51a339aa70dd421633e77469
Mirosław Buszek <zoowett@gmail.com>
2019-09-25 21:15:29 +0200

    Ad user choice

--- src/com/mbuszek/Main.java
+++ src/com/mbuszek/Main.java
@@ -1,9 +1,42 @@
 package com.mbuszek;
 
+import com.mbuszek.enums.City;
+import com.mbuszek.enums.Movies;
+import com.mbuszek.logic.Order;
+import com.mbuszek.models.Cinema;
+import com.mbuszek.models.Cinemas;
+import com.mbuszek.models.Screening;
+import com.mbuszek.utils.UserActivities;
+
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Cinemas cinemaSystem = new Cinemas();
+
+        Cinema warsaw = cinemaSystem.addCinema(City.WARSZAWA);
+        Screening screening = warsaw.addSeans(Movies.MATRIX, LocalDateTime.of(2019, 10, 12, 15, 30), 10);
+
+        Order order = new Order(cinemaSystem);
+        order.buyTicket(screening, false, 0);
+        order.buyTicket(screening, true, 1);
+        order.buyTicket(screening, false, 4);
+
+        System.out.println();
+
+        System.out.println(screening);
+
+        System.out.println();
+
+        order.orderCancel(screening, 0);
+        order.orderConfirmation(screening, 1);
+        System.out.println();
+        System.out.println(screening);
+        System.out.println();
+        UserActivities userAct = new UserActivities();
+        userAct.userActivities();
 
     }
 }
--- src/com/mbuszek/utils/UserActivities.java
+++ src/com/mbuszek/utils/UserActivities.java
@@ -1,4 +1,23 @@
 package com.mbuszek.utils;
 
+import java.util.Scanner;
+
 public class UserActivities {
+
+    public void userActivities() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select an option");
+        System.out.println("'1' > book a ticket");
+        System.out.println("'2' > confirm payment");
+        System.out.println("'3 > cancel payment");
+        String typeOfProcess = scanner.next();
+
+        if (typeOfProcess.equals("1")) {
+            System.out.println("Select a screening");
+            String screening = scanner.next();
+            System.out.println("Select a place ID");
+            int id = scanner.nextInt();
+        }
+    }
 }
